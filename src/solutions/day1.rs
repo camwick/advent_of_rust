85658ce3b81ad13476ff5c7dfc54913b4cd49919
camwick <cwickers@pnw.edu>
2024-12-07 13:47:34 -0500

    redo day1 part2  with a frequency map

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -5,8 +5,8 @@ use std::io::{BufRead, BufReader};
 pub fn run_part1() {
     let filename = "./input/y2024/day1/part1_input.txt";
 
-    let mut list_one: Vec<i32> = Vec::new();
-    let mut list_two: Vec<i32> = Vec::new();
+    let mut list_one: Vec<i64> = Vec::new();
+    let mut list_two: Vec<i64> = Vec::new();
     get_lists(filename, &mut list_one, &mut list_two);
 
     list_one.sort();
@@ -24,30 +24,30 @@ pub fn run_part1() {
 
 pub fn run_part2() {
     let filename = "./input/y2024/day1/part1_input.txt";
+    // incorrect attempt: 2403431
 
-    let mut list_one: Vec<i32> = Vec::new();
-    let mut list_two: Vec<i32> = Vec::new();
+    let mut list_one: Vec<i64> = Vec::new();
+    let mut list_two: Vec<i64> = Vec::new();
     get_lists(filename, &mut list_one, &mut list_two);
 
-    // create hashmap where key is the list_one entry and key is the count existing in list_two
-    let mut similarity_scores = HashMap::new();
-    for key in list_one.iter() {
-       if list_two.contains(key) {
-           similarity_scores.entry(key).and_modify(|e| { *e += 1 }).or_insert(1);
-       }
+    // create a frequency map to total the occurences of numbers in list two
+    let mut frequency_map: HashMap<i64, i64> = HashMap::new();
+    for &num in &list_two {
+        *frequency_map.entry(num).or_insert(0) += 1;
     }
 
-    // now calculate the similarity score
+    // calculate the similarity score by finding the frequency of a number from list 1 in list 2
     let mut similarity_score: i64 = 0;
-    for entry in list_one.iter() {
-        let score = entry * similarity_scores.get(entry).copied().unwrap_or(0);
-        similarity_score += i64::from(score);
+    for &num in &list_one {
+        if let Some(&count) = frequency_map.get(&num) {
+            similarity_score += num * count;
+        }
     }
 
     println!("Similarity score: {}", similarity_score);
 }
 
-fn get_lists(input_file: &str, list_one: &mut Vec<i32>, list_two: &mut Vec<i32>) {
+fn get_lists(input_file: &str, list_one: &mut Vec<i64>, list_two: &mut Vec<i64>) {
     let file = File::open(input_file).expect("Unable to open file");
     let reader = BufReader::new(file);
 
@@ -56,7 +56,7 @@ fn get_lists(input_file: &str, list_one: &mut Vec<i32>, list_two: &mut Vec<i32>)
         let parts: Vec<&str> = line.split(' ').collect();
 
         // rust's split leaves the white space... sigh
-        list_one.push(parts[0].parse::<i32>().unwrap());
-        list_two.push(parts[3].parse::<i32>().unwrap());
+        list_one.push(parts[0].parse::<i64>().unwrap());
+        list_two.push(parts[3].parse::<i64>().unwrap());
     }
 }
