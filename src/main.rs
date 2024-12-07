e8204a5d31ce2a1d4f056fb282b15d468cef72ea
camwick <cwickers@pnw.edu>
2024-12-07 13:47:14 -0500

    use cli input to determine the day to run

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 mod solutions;
+
+use std::io;
+
 use solutions::day1;
 use solutions::day7;
 
 fn main() {
-    day1::run_part1();
-    day1::run_part2();
-    println!();
-    day7::run_part1();
-    day7::run_part2();
+    println!("Enter number of day to run");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let day_number: i32 = input.trim().parse().expect("Failed to parse day number");
+
+    if day_number == 1 {
+        day1::run_part1();
+        day1::run_part2();
+    }
+    else if day_number == 7 {
+        day7::run_part1();
+        day7::run_part2();
+    }
 }
+
