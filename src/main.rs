130c3bf9f3898df275ef13e306061f1633e59424
camwick <cwickers@pnw.edu>
2024-12-08 10:03:58 -0500

    wrong day name...

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::io;
 
 use solutions::day1;
 use solutions::day2;
-use solutions::day3;
 use solutions::day7;
+use solutions::day8;
 
 fn main() {
     println!("Enter number of day to run");
@@ -22,13 +22,14 @@ fn main() {
     if day_number == 2 {
         day2::run_part1();
     }
-    if day_number == 3 {
-        day3::run_part1();
-        day3::run_part2();
     }
     else if day_number == 7 {
         day7::run_part1();
         day7::run_part2();
+    }
+    if day_number == 8 {
+        day8::run_part1();
+        day8::run_part2();
     }
 }
 
